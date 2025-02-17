024ed560a930f3b8ff50fa640ff2e6dcb7439e20
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-17 17:08:36 +0100

    first attemt to learn delegates

--- MoreAdvancedMethodsAndFunctions/Program.cs
+++ MoreAdvancedMethodsAndFunctions/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Choose an example to run.");
+Action action = NicksAction;
+
+// now we can call the method by invoking the variable:
+action();
+action.Invoke(); // either way works!
+
+void NicksAction()
+{
+    Console.WriteLine("Hello from Nick!");
+}
